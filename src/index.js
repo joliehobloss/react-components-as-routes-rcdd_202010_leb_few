0dbe15dd463486b649cbd38d2b2190ee5504887b
Jolie Hobloss <jolieshobloss@gmail.com>
2021-01-13 11:17:03 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -42,9 +42,11 @@ const Login = () => {
 
   ReactDOM.render((
   <Router>
+    <div>
       <Route path="/" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/login" component={Login} />
+    </div>
   </Router>),
   document.getElementById('root')
 );
